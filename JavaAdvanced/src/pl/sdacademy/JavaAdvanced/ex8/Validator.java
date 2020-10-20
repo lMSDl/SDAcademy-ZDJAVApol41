182dc34db6ec4b64b9e5386ace0a17076cc1e377
Paweł Łęczycki <pawel@msd.info.pl>
2020-10-20 19:51:05 +0200

    refactor

--- JavaAdvanced/src/pl/sdacademy/JavaAdvanced/ex8/Validator.java
+++ JavaAdvanced/src/pl/sdacademy/JavaAdvanced/ex8/Validator.java
@@ -2,18 +2,17 @@ package pl.sdacademy.JavaAdvanced.ex8;
 
 public class Validator implements IValidator {
 
+    public final static int MAX_DIMENSION_SUM = 300;
+    public final static int MAX_DIMENSION = 30;
+    public final static float MAX_EXPRESS_WEIGHT = 15;
+    public final static float MAX_WEIGHT = 30;
 
     @Override
     public boolean validate(Parcel input) {
-        if (input.getxLength() + input.getyLength() + input.getzLength() <= 300) {
-            if (input.getxLength() <= 30 && input.getzLength() <= 30 && input.getyLength() <= 30) {
-                if (input.isExpress()) {
-                    if (input.getWeight() <= 15) {
-                        return true;
-                    }
-                } else {
-                    if (input.getWeight() <= 30)
-                        return true;
+        if (input.getxLength() + input.getyLength() + input.getzLength() <= MAX_DIMENSION_SUM) {
+            if (input.getxLength() <= MAX_DIMENSION && input.getzLength() <= MAX_DIMENSION && input.getyLength() <= MAX_DIMENSION) {
+                if(input.getWeight() <= (input.isExpress() ? MAX_EXPRESS_WEIGHT : MAX_WEIGHT)) {
+                    return true;
                 }
             }
         }
