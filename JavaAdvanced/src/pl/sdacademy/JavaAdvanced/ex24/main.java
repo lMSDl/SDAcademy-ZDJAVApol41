98d323213df0411185d6dc41da6a8f3f0e4767c4
Michał Gorczyński <m.gorczynski94@gmail.com>
2020-10-26 18:09:15 +0100

    zad 24 runTimeException

---
+++ JavaAdvanced/src/pl/sdacademy/JavaAdvanced/ex24/main.java
@@ -0,0 +1,14 @@
+package pl.sdacademy.JavaAdvanced.ex24;
+
+public class main {
+    public static void main(String[] args) {
+
+        Basket basket = new Basket();
+
+        for (int i = 0; i < 12; i++) {
+            basket.addToBasket();
+        }
+
+
+    }
+}
